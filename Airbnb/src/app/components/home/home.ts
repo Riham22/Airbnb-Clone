abb186cdc5ea609a40622188039119b20983ec40
Riham <rruwainy@gmail.com>
2025-11-16 10:57:50 +0200

    initiating nav,home,footer

--- Airbnb/src/app/components/home/home.ts
+++ Airbnb/src/app/components/home/home.ts
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
+export class HomeComponent {
+listings = [
+{ title: 'Cozy Apartment', price: 120, image: 'assets/img1.jpg' },
+{ title: 'Beach House', price: 240, image: 'assets/img2.jpg' },
+{ title: 'Mountain Cabin', price: 180, image: 'assets/img3.jpg' }
+];
+filters = [
+{ name: 'Beach', icon: '🏖️' },
+{ name: 'Rooms', icon: '🛏️' },
+{ name: 'Trending', icon: '🔥' },
+{ name: 'Castles', icon: '🏰' },
+{ name: 'Islands', icon: '🏝️' },
+{ name: 'Cabins', icon: '🌲' },
+];
 }
